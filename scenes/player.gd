c87cd905db911e83ace1842d539d50d151476ebe
tnorrisoliva <tcno.ae42@gmail.com>
2025-10-27 15:01:41 -0400

    create character controller

---
+++ scenes/player.gd
@@ -0,0 +1,44 @@
+extends CharacterBody3D
+
+
+const SPEED = 4.0
+const JUMP_VELOCITY = 4.5
+@onready var neck :=$neck
+@onready var camera :=$neck/Camera3D
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton :
+		#locks mouse
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	elif event.is_action_pressed("ui_cancel"):
+		#unlocks mouse
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		if event is InputEventMouseMotion:
+			neck.rotate_y(-event.relative.x *0.01)
+			camera.rotate_x(-event.relative.y *0.01)
+			camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-30), deg_to_rad(60))
+	
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir := Input.get_vector("move_left", "move_right", "move_foward", "move_back")
+	#added .neck it computes directio nased on rotation of neck node rather than the orginal position of the player
+	var direction = (neck.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
