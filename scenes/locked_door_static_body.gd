62b8300e9076508b65b74bc821ed2cd8652d7715
tnorrisoliva <tcno.ae42@gmail.com>
2025-10-27 20:57:17 -0400

    keys open doors now

---
+++ scenes/locked_door_static_body.gd
@@ -0,0 +1,25 @@
+extends StaticBody3D
+
+@export var required_key: String = "gold_key"
+var is_open = false
+
+func interact(player):
+	if is_open:
+		return
+	if required_key in player.inventory:
+		print("door unlocked with", required_key)
+		open_door()
+	else:
+		print("you need the", required_key)
+func open_door():
+	is_open = true
+	queue_free()
+	$"..".hide() # might delete depends on feedback
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
